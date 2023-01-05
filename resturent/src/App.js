c01baf04eabed74eeb242ee6defc47f060f89d97
himesh <himeshkashyap347@gmail.com>
2023-01-04 23:43:34 -0800

    resturent website task 1

---
+++ resturent/src/App.js
@@ -0,0 +1,16 @@
+import React,{Fragment} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './component/Layout/Header';
+
+
+
+function App() {
+  return (
+    <Fragment >
+     <Header/>
+    </Fragment>
+  );
+}
+
+export default App;
