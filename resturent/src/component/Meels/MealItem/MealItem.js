21f706abb7fa8f34a4c9bc4cd5bfebbee54aa6b8
himesh <himeshkashyap347@gmail.com>
2023-01-05 05:15:52 -0800

    resturent website task  4

--- resturent/src/component/Meels/MealItem/MealItem.js
+++ resturent/src/component/Meels/MealItem/MealItem.js
@@ -1,4 +1,5 @@
 import classes from './MealItem.module.css';
+import MealItemForm from './MealItemForm';
 
 
 const MealItem =props=>{
@@ -8,8 +9,10 @@ const MealItem =props=>{
             <h3>{props.name}</h3>
             <div className={classes.description}>{props.description}</div>
             <div className={classes.price}>{price}</div>
-
-           </div>
+            </div>
+            <div>
+               <MealItemForm/>
+            </div>
       </li>
 }
 
